ddf3c9c1553f3c9c6d73ca8daff38510a06dcf09
Cyprienhr <cyprienhagena01@gmail.com>
2023-07-05 19:20:13 +0200

    all done

--- script.js
+++ script.js
@@ -6,17 +6,45 @@ function getComputerChoice(){
 }
 function playRound(computerSelection , playerSelection){
     if ( computerSelection === "rock" && playerSelection ==="sissors"
-       || computerSelection === "paper" && playerSelection === "rock" ){
+       || computerSelection === "paper" && playerSelection === "rock"
+       || computerSelection === "sissors"  && playerSelection === "paper"){
         return "you lose! "+computerSelection+" beats "+playerSelection;
        }
        else if(playerSelection === "rock" && computerSelection ==="sissors"
-       || playerSelection === "paper" && computerSelection === "rock" ){
+       || playerSelection === "paper" && computerSelection === "rock"
+       || playerSelection === "sissors"  && computerSelection === "paper" ){
         return "you win ! "+playerSelection+" beats "+computerSelection;
        }
        else{
-        return "Tie";
+        return "Draw";
        }
 }
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("enter your weapon rock , paper or sissors");
-console.log( playRound(computerSelection,playerSelection));
+
+
+function game()
+{
+    let computerScore = 0;
+    let playerScore = 0;
+    for( let i =0 ; i<5;i++){
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("enter your weapon rock , paper or sissors");
+        let result = playRound(computerSelection,playerSelection);
+        console.log(result);
+        if(result.startsWith("you lose! ")){
+            computerScore++ ;
+        }
+        else if(result.startsWith("you win ! ")){
+            playerScore++;
+        }
+    }
+  if (playerScore > computerScore){
+    console.log("CONGLATULATIONS! YOU WON THE GAME");
+  }
+  else if (computerScore > playerScore){
+    console.log("SORRY YOU LOSE THE GAME");
+  }
+  else{
+    console.log("GAME IS DRAW");
+  }
+}
+game();
